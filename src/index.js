fc07389f87441093e7cda691bda0094c1b27dd41
ChrisGCampbell <chris@chrisgcampbell.com>
2016-12-20 08:59:27 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Chris-Campbell on 12/19/2016.
+ */
+//Create a new component. This component should produce some html
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>kiss mee</div>;
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
+
