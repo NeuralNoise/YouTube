630d1d7f836e04f341f81a323c44e6366e36b8dd
ChrisGCampbell <chris@chrisgcampbell.com>
2016-12-24 12:20:16 -0500

    Developed video detail component

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,3 +1,24 @@
 /**
  * Created by Chris-Campbell on 12/20/2016.
  */
+import React from 'react';
+
+const VideoDetail = ({video}) => {
+
+    const videoId = video.id.videoId;
+    const url = `https://www.youtube.com/embed/${videoId}`;
+
+    return (
+        <div className="'video-detail col-md-8">
+            <div className="embed-responsive embed-responsive-16by9">
+                <iframe className="embed-responsive-item" src={url}></iframe>
+            </div>
+            <div className="details">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default VideoDetail;
