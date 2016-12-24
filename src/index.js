672c83e7e73e4d3060be1414b0bf268d632a68b6
ChrisGCampbell <chris@chrisgcampbell.com>
2016-12-24 12:31:10 -0500

    fixed errors in adding video detail component

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyBj7axV5Ep719GBzJKrTNgmPSBKzhlRBu4';
 
 
@@ -16,7 +17,7 @@ class App extends Component {
 
         this.state = { videos: [] };
 
-        YTSearch({key: API_KEY, term: 'Kangaroos'}, (videos) => {
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
             this.setState({ videos });
             //note: this.setState({ videos: videos });
         });
@@ -26,6 +27,7 @@ class App extends Component {
         return (
             <div>
               <SearchBar/>
+                <VideoDetail video={this.state.videos[0]}/>
                 <VideoList videos={this.state.videos}/>
             </div>
         );
