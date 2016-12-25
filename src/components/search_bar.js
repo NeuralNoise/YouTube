e378b98c56cbcf4a4a8ee4e2e359e6a92dd9dae4
ChrisGCampbell <chris@chrisgcampbell.com>
2016-12-24 19:09:21 -0500

    fix conflicts

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
     constructor(props) {
-        super(props);
+        super(prop);
 
         this.state = { term:'' };
     }
