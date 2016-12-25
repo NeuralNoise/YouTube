9172a03b8d6aaff9277d8127bb7b578cbef80944
ChrisGCampbell <chris@chrisgcampbell.com>
2016-12-25 13:21:42 -0500

    implemented search functinality

--- src/index.js
+++ src/index.js
@@ -20,7 +20,11 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.videoSearch('surboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
@@ -31,7 +35,7 @@ class App extends Component {
     render() {
         return (
             <div>
-              <SearchBar/>
+              <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
